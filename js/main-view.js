66b072c70394667eb7d92583b6bb1e0634f05062
AraceliGS <aragutarras@gmail.com>
2018-02-21 00:33:38 -0500

    añadiendo avances en el maquetado y la funcionalidad- vista splash, logo, plugin estrellas RateYo, etc.

--- js/main-view.js
+++ js/main-view.js
@@ -1,4 +1,153 @@
 $(document).ready(function() {
   $('.button-collapse').sideNav();
+  // Asignando estrellas
+  $(function() {
+    // Estrellas vista mobile
+    $('#rateYo-0-1').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-0-2').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-0-3').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-0-4').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-0-5').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 4.5,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-0-6').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 3.5,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-0-7').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 3,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-0-8').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-0-9').rateYo({
+      starWidth: '25px',
+      normalFill: '#A0A0A0',
+      rating: 3,
+      spacing: '5px',
+      halfStar: true,
+      readOnly: true
+    });
+    // Estrellas vista desktop
+    $('#rateYo-1').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 4, 
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-2').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-3').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-4').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-5').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 4.5,
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-6').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 3.5,
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-7').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 3,
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-8').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 4,
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+    $('#rateYo-9').rateYo({
+      starWidth: '35px',
+      normalFill: '#A0A0A0',
+      rating: 3,
+      spacing: '10px',
+      halfStar: true,
+      readOnly: true
+    });
+  });
 });
   
