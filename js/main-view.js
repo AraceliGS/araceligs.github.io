795cd4ada0a57bd2c1503a2df548896557061b4f
AraceliGS <aragutarras@gmail.com>
2018-02-21 22:55:52 -0500

    añadiendo modificaciones a los estilos

--- js/main-view.js
+++ js/main-view.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.button-collapse').sideNav();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
   // Asignando estrellas
   $(function() {
     // Estrellas vista mobile
