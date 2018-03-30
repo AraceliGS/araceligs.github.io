e1473b53c5ad5b3a6e5b1a7b7801bdd9fd4c777c
AraceliGS <aragutarras@gmail.com>
2018-03-30 00:54:45 -0500

    modificando el contenido de la seccion de habilidades blandas y los estilos de la seccion de habilidades tech, así como añadiendo el titulo d emis proyectos

--- js/main-view.js
+++ js/main-view.js
@@ -101,7 +101,7 @@ $(document).ready(function() {
     });
     // Estrellas vista desktop
     $('#rateYo-1').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 4, 
       spacing: '10px',
@@ -109,7 +109,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-2').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 4,
       spacing: '10px',
@@ -117,7 +117,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-3').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 4,
       spacing: '10px',
@@ -125,7 +125,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-4').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 4,
       spacing: '10px',
@@ -133,7 +133,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-5').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 4.5,
       spacing: '10px',
@@ -141,7 +141,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-6').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 3.5,
       spacing: '10px',
@@ -149,7 +149,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-7').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 4,
       spacing: '10px',
@@ -157,7 +157,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-8').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 4,
       spacing: '10px',
@@ -165,7 +165,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-9').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 4,
       spacing: '10px',
@@ -173,7 +173,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-10').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 3.5,
       spacing: '10px',
@@ -181,7 +181,7 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-11').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 3.5,
       spacing: '10px',
@@ -189,47 +189,13 @@ $(document).ready(function() {
       readOnly: true
     });
     $('#rateYo-12').rateYo({
-      starWidth: '35px',
+      starWidth: '30px',
       normalFill: '#A0A0A0',
       rating: 3.5,
       spacing: '10px',
       halfStar: true,
       readOnly: true
     });
-  });
-
-  var image1 = $('#soft-skill-1').attr('src');
-  var image2 = $('#soft-skill-2').attr('src');
-  var image3 = $('#soft-skill-3').attr('src');
-
-  $('.soft-skill-container').on('mouseover', '#soft-skill-1', function() {
-    $('#soft-skill-1').attr('src', '../assets/images/image4.jpg');
-    $('#soft-skill-1').removeClass('img.soft-skills-image');
-    $('#soft-skill-1').addClass('soft-skills-image-2'); 
-  });
-
-  $('.soft-skill-container').on('mouseover', '#soft-skill-2', function() {
-    $('#soft-skill-2').attr('src', '../assets/images/image5.jpg');
-    $('#soft-skill-2').removeClass('img.soft-skills-image');
-    $('#soft-skill-2').addClass('soft-skills-image-2');
-  });
-
-  $('.soft-skill-container').on('mouseover', '#soft-skill-3', function() {
-    $('#soft-skill-3').attr('src', '../assets/images/image6.jpg');
-    $('#soft-skill-3').removeClass('img.soft-skills-image');
-    $('#soft-skill-3').addClass('soft-skills-image-2');
-  });
- 
-  $('.soft-skill-container').on('mouseleave', function() {
-    $('#soft-skill-1').attr('src', image1);
-    $('#soft-skill-2').attr('src', image2);
-    $('#soft-skill-3').attr('src', image3);
-    $('#soft-skill-1').removeClass('soft-skills-image-2');
-    $('#soft-skill-2').removeClass('soft-skills-image-2');
-    $('#soft-skill-3').removeClass('soft-skills-image-2');
-    $('#soft-skill-1').addClass('img.soft-skills-image');
-    $('#soft-skill-2').addClass('img.soft-skills-image');
-    $('#soft-skill-3').addClass('img.soft-skills-image');
   });
 });
   
